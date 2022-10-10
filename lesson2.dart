56da5ab8a05c074ae2885217f46f174df2753b1f
Macbook <macbook@Macbooks-MacBook-Pro.local>
2022-10-10 20:36:29 +0600

    first commit

---
+++ lesson2.dart
@@ -0,0 +1,26 @@
+void main() {
+  int a = 9;
+  double d = 5.2;
+  print(d);
+  int b = 4;
+  print(a + b);
+
+  String msg = "hello friends";
+  print(msg);
+
+  bool isCar = true;
+  print(isCar);
+
+ List array=[3, 6, 8];
+ print(array);
+
+ Map student={'name': 'Adelya','age':'14'};
+ print(student);
+
+ var heart='\u2665';
+ print(heart);
+
+ dynamic number=12;
+ print(number);
+
+}
